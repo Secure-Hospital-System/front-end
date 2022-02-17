a6c2ebd5d2b5856b9d11465e8237232350ae16eb
Yash <4596yash@gmail.com>
2022-02-17 09:45:23 -0700

    Basic Patient Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hospital-system';
+  toppings = new FormControl();
+  one = 'one';
+  two = 'one';
+  selectedValue = '';
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  changeClient(value:any) {
+    console.log(value);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import { CreateAppointmentComponent } from './create-appointment/create-appointment.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+import { ReportsComponent } from './reports/reports.component';
+import { InsuranceComponent } from './insurance/insurance.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AppointmentComponent,
+    ToolbarComponent,
+    CreateAppointmentComponent,
+    ViewAppointmentComponent,
+    ReportsComponent,
+    InsuranceComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  // exports: [MaterialModule],
+  // providers: [MaterialModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
